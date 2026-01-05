62e8eb5952ca732582f1ee7a66baea1253340d20
yosket <yosket87@gmail.com>
2026-01-05 12:00:00 +0900

    Fix bug in Home.js

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Mainpage from "../../pages/Mainpage/Mainpage"
+
+export const Home = () => {
+  return (
+    <div>
+      <Mainpage />
+    </div>
+  )
+}
+
+export default Home
